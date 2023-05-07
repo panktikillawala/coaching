a5eafeb0e868e1cc0c8b49368887f1ee4ae7562d
Akhil Killawala <panktikillawalacoaching@gmail.com>
2023-05-07 11:16:15 -0700

    website

---
+++ js/interact-quiz-embed-interact-embed.js
@@ -0,0 +1,6 @@
+(function(d, t){
+	var s=d.createElement(t);
+	s.src='https://i.tryinteract.com/embed/app.js';
+	s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs)if(rs!='complete')if(rs!='loaded')return;};
+	var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr);
+})(document,'script');
